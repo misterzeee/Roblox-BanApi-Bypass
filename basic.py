f0a27ce2669cb3f374015d96165068cefb403121
misterzeee <cameronzaduck@gmail.com>
2026-06-02 01:38:42 +0100

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+import time
+import os
+
+print("Zee was here.")
+
+time.sleep(1)
+
+rbxcookiespath = f"{os.path.expanduser("~")}\\AppData\\Local\\Roblox\\LocalStorage\\RobloxCookies.dat"
+profilespath = f"{Path(__file__).parent.absolute()}\\Profiles"
+
+if not Path(f"{Path(__file__).parent.absolute()}\\Profiles").is_dir():
+    os.mkdir(f"{Path(__file__).parent.absolute()}\\Profiles")
+
+def clear_roblox_cookies():
+    with open(rbxcookiespath, "w") as f:
+        f.seek(0)
+        f.truncate()
+    print("Cleared Cookies")
+    return
+
+
+menu = """
+Note: This is the basic version of the menu, in the future I will add the verison with cloud/local account saves (when I get the time).
+
+01: Clear Roblox App Cookies
+
+99: Exit
+"""
+
+print(menu)
+
+while True:
+    option = int(input(": "))
+    if option == 1:
+        clear_roblox_cookies()
+    
+    if option == 99:
+        exit(1)
